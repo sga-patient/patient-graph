a303ec9a28cde1964d1810d4f0512ed3960d4e21
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-11-26 13:38:42 +0900

    update

--- src/hospitalManager.py
+++ src/hospitalManager.py
@@ -1,9 +1,17 @@
-def putInDB(filename):
-    with open(filename, 'r'):
+import db.neo4jDB as neo4jDB
+import neo4jController
 
+def putInDB(filename):
+    info = neo4jDB.getInfo()
+    controller = neo4jController.Neo4jController(info["url"], info["user"], info["pw"])
+    controller.connect()
+
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        print(lines)
 
 
 if __name__=="__main__":
     filename = "/Volumes/Seagate/workspace/sga-patient/patient-graph/data/hospital.csv"
-    
+
     putInDB(filename)
